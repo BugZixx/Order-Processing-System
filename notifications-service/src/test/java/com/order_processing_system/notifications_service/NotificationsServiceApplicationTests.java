c83abbd210dd9463753a44142539829b7ee79137
BugZixx <bruno.fm.ribeiroo@gmail.com>
2024-09-03 19:18:42 +0200

    first commit

---
+++ notifications-service/src/test/java/com/order_processing_system/notifications_service/NotificationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_processing_system.notifications_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/src/test/java/com/order_processing_system/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_processing_system.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
